486c6c08197d91bedd6e9eb584f1080a80bd8063
Jordan Alexander Cruz - AlexCG Design <jordanalexanderweb@gmail.com>
2022-06-27 12:12:29 -0500

    3 PROYECTOS

---
+++ Slider/sliders.js
@@ -0,0 +1,25 @@
+(function(){
+    const sliders = [...document.querySelectorAll('.slider__body')];
+    const arrowNext = document.querySelector('#next');
+    const arrowBefore = document.querySelector('#before');
+    let value;
+
+    arrowNext.addEventListener('click', ()=>changePosition(1));
+
+    arrowBefore.addEventListener('click', ()=>changePosition(-1));
+
+    function changePosition(change){
+        const currentElement = Number(document.querySelector('.slider__body--show').dataset.id);
+        // 4+1 = 5
+        value = currentElement;
+        value+= change;
+
+        console.log(sliders.length)
+        if(value === 0 || value == sliders.length+1){
+            value = value === 0 ? sliders.length : 1;
+        }
+
+        sliders[currentElement-1].classList.toggle('slider__body--show');
+        sliders[value-1].classList.toggle('slider__body--show');
+    }
+})()
